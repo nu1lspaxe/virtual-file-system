a1b65b838893bef81c52c216895d1df3529c1d5b
NU1LSPAXE\nu1ls <nu1lspaxe.anonym0ux@gmail.com>
2024-08-13 04:43:59 +0800

    [Feat] `help` command

--- main.go
+++ main.go
@@ -1,18 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"system/pkg"
 )
 
-func main() {
+func init() {
 	path, err := pkg.GetManPath()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
 	pkg.SetManInfo(path, "./vfs.1")
+}
 
-	fmt.Println("Running the main CLI application...")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var greetings = `
+Welcome to Virtual File System!
+Type 'help' to get details and 'exit' to leave.
+`
+
+	fmt.Print(greetings)
+	fmt.Print("$ ")
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		handleCommand(input)
+		fmt.Print("$ ")
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+	}
+}
+
+func handleCommand(input string) {
+	parts := strings.Fields(input)
+	if len(parts) == 0 {
+		return
+	}
+
+	command := parts[0]
+	switch command {
+	case "register":
+	case "create-folder":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "delete-folder":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "list-folders":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "rename-folder":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "create-file":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "delete-file":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "list-files":
+		fmt.Fprintln(os.Stderr, "Error: Not implement yet.")
+	case "help":
+		pkg.GetManInfo()
+	case "exit":
+		os.Exit(0)
+	default:
+		fmt.Fprintln(os.Stderr, "Error: Unknown command.")
+	}
 }
--- pkg/utils.go
+++ pkg/utils.go
@@ -8,7 +8,10 @@ import (
 	"strings"
 )
 
-var ValidChars = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+var (
+	ValidChars = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+	manFile    string
+)
 
 // GetManPath get the path of `man` command.
 func GetManPath() (string, error) {
@@ -27,15 +30,20 @@ func GetManPath() (string, error) {
 // SetManInfo is used to set man path and then run man [txtPath] to show deatils.
 func SetManInfo(manPath, txtPath string) {
 	os.Setenv("PATH", fmt.Sprintf("%v:", manPath)+os.Getenv("PATH"))
+	manFile = txtPath
 
 	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
-		cmd := exec.Command("man", txtPath)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			fmt.Fprintf(os.Stderr, "Error running man: %v\n", err)
-			os.Exit(1)
-		}
-		return
+		GetManInfo()
+		os.Exit(0)
+	}
+}
+
+func GetManInfo() {
+	cmd := exec.Command("man", manFile)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running man: %v\n", err)
+		os.Exit(1)
 	}
 }
