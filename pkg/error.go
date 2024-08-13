b880d0513c75bb68968bfcc05b689250a468c50a
NU1LSPAXE\nu1ls <nu1lspaxe.anonym0ux@gmail.com>
2024-08-13 22:51:54 +0800

    [Feat] Append RespondType and formation response

---
+++ pkg/error.go
@@ -0,0 +1,41 @@
+package pkg
+
+import "fmt"
+
+type RespondType int
+
+const (
+	Succeed RespondType = -1
+
+	ErrAlreadyExists RespondType = iota
+	ErrInvalidChars
+	ErrNotExists
+	ErrArgsLength
+	ErrUnknown
+
+	WarnNoFolders
+	WarnEmptyFolder
+)
+
+func (r RespondType) ToString(item ...string) string {
+	switch r {
+	case Succeed:
+		return ""
+	case ErrAlreadyExists:
+		return fmt.Sprintf("Error: The %v has already existed.", item)
+	case ErrInvalidChars:
+		return fmt.Sprintf("Error: The %v contain invalid chars.", item)
+	case ErrNotExists:
+		return fmt.Sprintf("Error: The %v doesn't exist.", item)
+	case ErrArgsLength:
+		return "Error: Invalid command syntax. Check `help` to get info!"
+	case ErrUnknown:
+		return "Unrecognized command."
+	case WarnNoFolders:
+		return fmt.Sprintf("Warning: The %v doesn't have any folders", item)
+	case WarnEmptyFolder:
+		return "Warning: The folder is empty."
+	default:
+		return "Undefined"
+	}
+}
